b984444561995ffe1a259ea8de6a3b42d8e678f1
Juan Pablo Molinas <juan.molinas@raona.com>
2022-05-20 14:01:00 -0300

    initial commit

---
+++ src/webparts/chartExample/components/ChartExample.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import styles from './ChartExample.module.scss';
+import { IChartExampleProps } from './IChartExampleProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export default class ChartExample extends React.Component<IChartExampleProps, {}> {
+  public render(): React.ReactElement<IChartExampleProps> {
+    const {
+      description,
+      isDarkTheme,
+      environmentMessage,
+      hasTeamsContext,
+      userDisplayName
+    } = this.props;
+
+    return (
+      <Bar options={options} data={data} />
+    );
+  }
+}
---
+++ src/webparts/chartExample/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IChartExampleWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ChartExampleWebPartStrings' {
+  const strings: IChartExampleWebPartStrings;
+  export = strings;
+}
